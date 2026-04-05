924d43c9905121ab5a790a8b5d817092e4b8b0cb
belendepalo <mdepalo@udesa.edu.ar>
2026-04-05 20:44:02 -0300

    Tests: Forecast & Wells
    Tests to evaluate the initial function of the endpoints: Forecast and Wells

---
+++ tests/tests_forecast.py
@@ -0,0 +1,129 @@
+"""Tests para endpoint de forecast."""
+
+import unittest
+from fastapi.testclient import TestClient
+from api.v1.security import get_api_key
+from api.v1.main import app
+
+route_forecast  = "/api/v1/forecast"
+
+class TestForecast(unittest.TestCase):
+    def setUp(self) -> None:
+        """Starts the client test"""
+        app.dependency_overrides[get_api_key] = lambda: "test-api-key"
+        self.client = TestClient(app)
+    
+    def tearDown(self) -> None:
+        """Cleans overrides to not affect other tests"""
+        app.dependency_overrides = {}
+
+    def test_hello(self) -> None:
+        """Checks that ``GET /hello`` returns hello"""
+        response = self.client.get("/hello")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"message": "Hola mundo!"})
+
+    def test_not_found_route(self):
+        """A non-existent route must return 404"""
+        response = self.client.get("/ruta-inexiste")
+        self.assertEqual(response.status_code, 404)
+    
+    def test_status_code_ok(self):
+        """GET /forecast with valid parameters must return 200"""
+        response = self.client.get(
+            route_forecast,
+            params={
+                "id_well": "POZO-001",
+                "date_start": "2026-05-23",
+                "date_end": "2026-05-25",
+            },
+        )
+        self.assertEqual(response.status_code, 200)
+
+    def test_expected_id_well(self):
+        """The return contains id_well written correctly"""
+        response = self.client.get(
+            route_forecast,
+            params={
+                "id_well": "POZO-001",
+                "date_start": "2026-05-23",
+                "date_end": "2026-05-25",
+            },
+        )
+        self.assertEqual(response.json()["id_well"], "POZO-001")
+    
+    def test_missing_id_well(self):
+        """If id_well fails, it must return 422"""
+        response = self.client.get(
+            route_forecast,
+            params={
+                "date_start": "2026-05-23",
+                "date_end": "2026-05-25",
+            },
+        )
+        self.assertEqual(response.status_code, 422)
+    
+    def test_missing_date_start(self):
+        """If date_start is missing, must return 422"""
+        response = self.client.get(
+            route_forecast,
+            params={
+                "id_well": "POZO-001",
+                "date_end": "2026-05-25",
+            },
+        )
+        self.assertEqual(response.status_code, 422)
+    
+    def test_missing_date_end(self):
+        """If date_end is missing, must return 422"""
+        response = self.client.get(
+            route_forecast,
+            params={
+                "id_well": "POZO-001",
+                "date_start": "2026-05-23",
+            },
+        )
+        self.assertEqual(response.status_code, 422)
+
+    def test_invalid_date(self):
+        """If date has incorrect format, must return 422"""
+        response = self.client.get(
+            route_forecast,
+            params={
+                "id_well": "POZO-001",
+                "date_start": "fecha-invalida",
+                "date_end": "2026-03-10",
+            },
+        )
+        self.assertEqual(response.status_code, 422)
+
+    def test_verb_not_allowed(self):
+        """POST /forecast is not allowed, must return 405"""
+        response = self.client.post(
+            route_forecast,
+            params={
+                "id_well": "POZO-001",
+                "date_start": "2026-05-23",
+                "date_end": "2026-05-25",
+            },
+        )
+        self.assertEqual(response.status_code, 405)
+
+    def test_exact_response_structure(self):
+        """Check correrct response structure"""
+        response = self.client.get(
+            route_forecast,
+            params={
+                "id_well": "POZO-001",
+                "date_start": "2026-05-23",
+                "date_end": "2026-05-25",
+            },
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {
+                "id_well": "POZO-001",
+                "data": [
+                    {"date": "2026-05-23", "prod": 150.5},
+                    {"date": "2026-05-25", "prod": 160.2},
+                ],
+            })
