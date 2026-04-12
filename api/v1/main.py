5f2dd11bddbe80712a536b4da58880a1acee1b2e
belendepalo <mdepalo@udesa.edu.ar>
2026-04-12 17:37:17 -0300

    connection between api/prometheus/grafana

--- api/v1/main.py
+++ api/v1/main.py
@@ -5,11 +5,14 @@ from fastapi import FastAPI
 
 from api.v1.forecast.routes import router as forecast_router
 from api.v1.wells.routes import router as wells_router
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI(title="Oil & Gas Forecast API")
 
 app.include_router(forecast_router, prefix="/api/v1")
 app.include_router(wells_router, prefix="/api/v1")
+
+Instrumentator().instrument(app).expose(app)
 
 @app.get("/hello")
 def root() -> Dict[str, str]:
